ee3be3eb730ebff4f24c7a1b967742976317c578
TD <tengdai722@gmail.com>
2024-04-23 23:46:51 -0700

    04/23/2024 Daily: 1137

---
+++ daily/1137 n-th-tribonacci-number.java
@@ -0,0 +1,26 @@
+/*
+https://leetcode.com/problems/n-th-tribonacci-number/
+
+标准DP
+
+Time: O(n)
+Space: O(1)
+*/
+
+class Solution {
+    public int tribonacci(int n) {
+        if (n < 3) {
+            return n > 0 ? 1 : 0;
+        }
+        
+        int a = 0, b = 1, c = 1;
+        for (int i = 0; i < n - 2; ++i) {
+            int tmp = a + b + c;
+            a = b;
+            b = c;
+            c = tmp;
+        }
+        
+        return c;
+    }
+}
