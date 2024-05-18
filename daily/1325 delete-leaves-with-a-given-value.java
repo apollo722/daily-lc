6f72d7995f267b4894c0a0087b3b3de3dcb67ff3
TD <tengdai722@gmail.com>
2024-05-18 12:40:55 -0700

    05/18/2024 Daily: 1325

---
+++ daily/1325 delete-leaves-with-a-given-value.java
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/problems/delete-leaves-with-a-given-value/
+
+递归模板，后续遍历
+
+Time: O(n)
+Space: O(n)
+*/
+
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null) return null;
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+        if (root.left == null && root.right == null && root.val == target) return null;
+        return root;
+    }
+}
