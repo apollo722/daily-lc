4ba33994fc33d2d811a3607df57c40654f79c739
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-02-24 21:27:13 -0800

    Time: 2 ms (91.07%), Space: 41.7 MB (42.39%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -10,7 +10,7 @@ class Solution {
                 i++;
                 count++;
             }
-            res.append(count + "");
+            res.append(count);
             res.append(prev.charAt(idx));
             idx = i;
         }
