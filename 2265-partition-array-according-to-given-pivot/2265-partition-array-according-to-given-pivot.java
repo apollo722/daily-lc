5b8bb71180aec6f05d2454d75f58fc5635dfa4b2
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-02 19:38:24 -0800

    Time: 4 ms (100%), Space: 68.6 MB (5.53%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -3,13 +3,23 @@ class Solution {
         int n = nums.length;
         int[] res = new int[n];
         int l = 0, r = n - 1;
-        for (int cur : nums) {
-            if (cur < pivot) res[l++] = cur;
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+            if (nums[i] < pivot) res[l++] = nums[i];
+            if (nums[j] > pivot) res[r--] = nums[j];
         }
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] > pivot) res[r--] = nums[i];
+        while (l <= r) {
+            res[l++] = pivot;
         }
-        for (int i = l; i <= r; i++) res[i] = pivot;
         return res;
     }
 }
+
+
+/*
+乍一看像是quick select，但实际上partition之后元素都要保持原有的顺序，就不能用quick select。
+最简单的想法就是创建一个结果数组，之后分别正向以及逆向扫一遍数组，分别将比pivot小和大的元素都放到结果数组的首尾。
+剩下的部分全用pivot填充。这样要正反扫两边数组。
+再优化一点点就是正反向同时扫，在一个循环里分别处理首尾。因为是分别处理大于和小于pivot的部分，所以不会相互干扰。
+最后再处理中间的相等部分。
+本题应该是没有办法不用额外的空间来解决，因为没有办法不破坏原有的顺序。
+*/
