19f7e870fc05f1d3973361f0da32d5c9a4b58298
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-02 22:25:28 -0800

    Time: 13 ms (75.33%), Space: 66.3 MB (60.2%) - LeetHub

--- 2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.java
+++ 2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.java
@@ -3,33 +3,33 @@ class Solution {
         int[] curState = new int[]{0, -1};
         int[] res = new int[arrival.length];
         int idx = 0;
-        HashMap<Integer, Deque<Integer>> m = new HashMap<>();
-        m.put(0, new ArrayDeque<>());
-        m.put(1, new ArrayDeque<>());
+        ArrayDeque<Integer>[] m = new ArrayDeque[2];
+        m[0] = new ArrayDeque<Integer>();
+        m[1] = new ArrayDeque<Integer>();
         while (idx < arrival.length) {
             while (idx < arrival.length && arrival[idx] <= curState[0]) {
-                m.get(state[idx]).add(idx);
+                m[state[idx]].add(idx);
                 idx++;
             }
             boolean tick = false;
-            while (m.get(0).size() > 0 || m.get(1).size() > 0) {
+            while (m[0].size() > 0 || m[1].size() > 0) {
                 tick = true;
                 if (curState[1] == -1) {
-                    if (m.get(1).size() > 0) {
-                        int curIdx = m.get(1).poll();
+                    if (m[1].size() > 0) {
+                        int curIdx = m[1].poll();
                         res[curIdx] = curState[0];
                         curState[1] = 1;
                     } else {
-                        int curIdx = m.get(0).poll();
+                        int curIdx = m[0].poll();
                         res[curIdx] = curState[0];
                         curState[1] = 0;
                     }
                 } else {
-                    if (m.get(curState[1]).size() > 0) {
-                        int curIdx = m.get(curState[1]).poll();
+                    if (m[curState[1]].size() > 0) {
+                        int curIdx = m[curState[1]].poll();
                         res[curIdx] = curState[0];
                     } else {
-                        int curIdx = m.get(1 - curState[1]).poll();
+                        int curIdx = m[1 - curState[1]].poll();
                         res[curIdx] = curState[0];
                         curState[1] = state[curIdx];
                     }
