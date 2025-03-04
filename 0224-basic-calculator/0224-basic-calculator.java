4d00fcd1270b97aec8642aee0bc3c46f3c18e425
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-03 23:06:28 -0800

    Time: 2 ms (96.79%), Space: 43.7 MB (72.27%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,23 +1,18 @@
 class Solution {
+    int idx = 0;
     public int calculate(String s) {
         char ops = '+';
         int curSum = 0, prev = 0, res = 0;
-        int i = 0, n = s.length();
-        while (i < n) {
-            char c = s.charAt(i++);
+        int n = s.length();
+        while (idx < n) {
+            char c = s.charAt(idx++);
             if (c == ' ') continue;
             else if (Character.isDigit(c)) {
                 curSum = 10 * curSum + c - '0';
             } else if (c == '(') {
-                int j = i, balance = 1;
-                while (balance > 0) {
-                    if (s.charAt(j) == '(') balance++;
-                    else if (s.charAt(j) == ')') balance--;
-                    j++;
-                }
-                curSum += calculate(s.substring(i, j));
-                i = j;
-            } else {
+               curSum = calculate(s);
+            } else if (c == ')') break;
+            else {
                 if (ops == '+') {
                     res += curSum;
                 } else res -= curSum;
