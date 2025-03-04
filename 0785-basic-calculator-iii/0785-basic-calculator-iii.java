a44f5ba5628320d3ade0fcf3607ab0a483b3fdb8
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-03 23:24:15 -0800

    Time: 1 ms (97.8%), Space: 41.7 MB (85.2%) - LeetHub

---
+++ 0785-basic-calculator-iii/0785-basic-calculator-iii.java
@@ -0,0 +1,35 @@
+class Solution {
+    int idx = 0;
+    public int calculate(String s) {
+        char ops = '+';
+        int num = 0, prev = 0, res = 0;
+        int n = s.length();
+        for ( ; idx < n; idx++) {
+            char c = s.charAt(idx);
+            if (Character.isDigit(c)) {
+                num = 10 * num + c - '0';
+            } 
+            if (c == '(') {
+                idx++;
+                num = calculate(s);
+            } 
+            if ((!Character.isDigit(c) && c != ' ') || idx == n - 1) {
+                if (ops == '+') {
+                    res += prev;
+                    prev = num;
+                } else if (ops == '-') {
+                    res += prev;
+                    prev = -num;
+                } else if (ops == '*') {
+                    prev *= num;
+                } else if (ops == '/') {
+                    prev /= num;
+                }
+                num = 0;
+                ops = c;
+            }
+            if (c == ')') break;
+        }
+        return res + prev;
+    }
+}
