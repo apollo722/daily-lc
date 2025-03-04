dac3bd7f0913b3bd0caadbf6dd91872f543f3703
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-04 10:25:00 -0800

    Time: 17 ms (92.51%), Space: 45.1 MB (91.99%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,7 +1,7 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         /*
-        int m = text1.length(), n = text2. length();
+        int m = text1.length(), n = text2.length();
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
@@ -15,7 +15,7 @@ class Solution {
         return dp[m][n];
         */
 
-        int m = text1.length(), n = text2. length();
+        int m = text1.length(), n = text2.length();
         int[] prev = new int[n + 1];
         for (int i = 1; i <= m; i++) {
             int[] cur = new int[n + 1];
