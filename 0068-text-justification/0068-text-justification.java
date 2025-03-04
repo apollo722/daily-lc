8e84720783c1b1ccca0d31b3703fa82138053a85
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-03 20:00:29 -0800

    Time: 0 ms (100%), Space: 41.8 MB (59.32%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -3,38 +3,49 @@ class Solution {
         int n = words.length, cur = 0;
         List<String> res = new ArrayList<>();
         while (cur < n) {
-            int curLen = 0, wordCnt = 0, i = cur, wordLen = 0;
-            while (cur < n && curLen + words[cur].length() <= maxWidth) {
-                wordLen += words[cur].length();
-                curLen += words[cur].length();
-                wordCnt++;
-                cur++;
-                if (curLen == maxWidth || cur == n) break;
-                curLen++;
+            int curLen = words[cur].length();
+            int last = cur + 1;
+            while (last < n && curLen + words[last].length() + (last - cur) <= maxWidth) {
+                curLen += words[last].length();
+                last++;
             }
+            int gaps = last - cur - 1;
             StringBuilder sb = new StringBuilder();
-            if (cur == n) {
-                while (wordCnt-- > 0) {
-                    sb.append(words[i++]);
-                    if (wordCnt > 0) sb.append(" ");
-                    else {
-                        for (int j = 0; j < maxWidth - curLen; j++) sb.append(" ");
+            if (last == n || gaps == 0) {
+                for (int i = cur; i < last; i++) {
+                    sb.append(words[i]).append(" ");
+                }
+                sb.deleteCharAt(sb.length() - 1);
+                while (sb.length() < maxWidth) sb.append(" ");
+            } else {
+                int spaces = (maxWidth - curLen) / gaps;
+                int extraSpaces = (maxWidth - curLen) % gaps;
+                for (int i = cur; i < last; i++) {
+                    sb.append(words[i]);
+                    if (i < last - 1) {
+                        int spacesToAdd = spaces + (extraSpaces-- > 0 ? 1 : 0);
+                        sb.append(" ".repeat(spacesToAdd));
                     }
                 }
-            } else {
-                int spaceCnt = wordCnt - 1, spaceLeft = maxWidth - wordLen;
-                int nextFill = spaceCnt == 0 ? 0 : (int) Math.ceil(spaceLeft * 1.0 / spaceCnt);
-                while (wordCnt-- > 0) {
-                    sb.append(words[i++]);
-                    for (int j = 0; j < nextFill; j++) sb.append(" ");
-                    spaceCnt--;
-                    spaceLeft -= nextFill;
-                    nextFill = spaceCnt == 0 ? 0 : (int) Math.ceil(spaceLeft * 1.0 / spaceCnt);
-                }
-                for (int j = 0; j < spaceLeft; j++) sb.append(" ");
             }
             res.add(sb.toString());
+            cur = last;
         }
         return res;
     }
 }
+
+
+/*
+又是逻辑清楚写起来不好写的题目。
+整道题只需要做两件事情：
+1. 看这一行有多少单词；
+2. 看这一行的空格怎么分配。
+
+对于1， 只要能装下就往行里装，要注意两个单词间至少要有一个空格。
+装满了单词之后，单词之间的gap就是单词个数-1。
+如果这行只有一个单词，或者这是最后一行了，即单词都分配完了，那么把单词都堆在左边，剩下的用空格填满即可。
+反之，开始计算2。
+公式是每一个gap至少要分到S/G个空格，而前S%G个gap要多分到1个空格，其中S是总共需要分多少个spaces，G是gap数量。
+这是因为每个空隙都要尽量的相接近，所以多余的空格要平摊给每一个空隙，从左至右。那么多余的空格就是S%G，总共能分S%G次。
+*/
