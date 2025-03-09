fd95e3fc421e14eb48912ddccc6a1e2c900d33e6
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-08 22:08:14 -0800

    Time: 20 ms (34.1%), Space: 46.2 MB (80.48%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -8,22 +8,26 @@ class Solution {
         for (int i = 0; i < m; i++) {
             pq.add(new int[]{i, 0, heightMap[i][0]});
             pq.add(new int[]{i, n - 1, heightMap[i][n - 1]});
+            visited[i][0] = true;
+            visited[i][n - 1] = true;
         }
         for (int i = 1; i < n - 1; i++) {
             pq.add(new int[]{0, i, heightMap[0][i]});
             pq.add(new int[]{m - 1, i, heightMap[m - 1][i]});
+            visited[0][i] = true;
+            visited[m - 1][i] = true;
         }
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
             int x = cur[0], y = cur[1], h = cur[2];
-            if (visited[x][y]) continue;
-            visited[x][y] = true;
-            if (curLevel < h) curLevel = h;
-            res += curLevel - heightMap[x][y];
+
             for (int d = 0; d < 4; d++) {
                 int i = x + dir[d], j = dir[d + 1] + y;
-                if (i >= 0 && i < m && j >= 0 && j < n && !visited[i][j] ) {
-                    pq.add(new int[]{i, j, heightMap[i][j]});
+                if (i >= 0 && i < m && j >= 0 && j < n && !visited[i][j]) {
+                    visited[i][j] = true;
+                    int nextHeight = heightMap[i][j];
+                    if (h > heightMap[i][j]) res += h - nextHeight;
+                    pq.add(new int[]{i, j, Math.max(nextHeight, h)});
                 }
             }
         }
