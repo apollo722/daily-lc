b61358caf33add00d4a1de4b37eb4b951ed8c778
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-10 00:47:08 -0700

    Time: 2 ms (83.02%), Space: 63.2 MB (47.43%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -1,27 +1,25 @@
 class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
-        List<Integer> gaps = new ArrayList<>();
-        if (startTime[0] - 0 > 0) gaps.add(startTime[0] - 0);
-        gaps.add(0);
-        for (int i = 1; i < startTime.length; i++) {
-            if (i > 0 && startTime[i] - endTime[i - 1] > 0) {
-                gaps.add(startTime[i] - endTime[i - 1]);
-            } 
-            gaps.add(0);
+        int size = startTime.length;
+        int[] gaps = new int[size + 1];
+
+        for (int i = 0; i < size; i++) {
+            int diff = (i == 0) ? startTime[i] : startTime[i] - endTime[i - 1];
+            gaps[i] = diff;
         }
-        if (eventTime > endTime[endTime.length - 1]) gaps.add(eventTime - endTime[endTime.length - 1]);
-        int res = 0, curSum = 0, l = 0, r = 0, n = gaps.size();
-        while (r < n) {
-            int cur = gaps.get(r++);
-            if (cur == 0) k--;
-            curSum += cur;
-            while (k < 0) {
-                int curLeft = gaps.get(l++);
-                if (curLeft == 0) k++;
-                curSum -= curLeft;
-            }
-            res = Math.max(res, curSum);
+        
+        gaps[size] = eventTime - endTime[size - 1];
+        
+        int cnt = k + 1, res = 0, l = 0, r = cnt, sum = 0;
+        for (int i = 0; i <= size && i < cnt; i++) {
+            sum += gaps[i];
         }
+        res = Math.max(res, sum);
+        for (int i = cnt; i <= size; i++) {
+            sum += gaps[i] - gaps[i - cnt];
+            res = Math.max(res, sum);
+        }
+        
         return res;
     }
 }
