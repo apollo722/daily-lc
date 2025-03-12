065701d0b77a5d3cc0d1f80e3822d7b5eaf61f42
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-11 23:00:34 -0700

    Time: 42 ms (84.67%), Space: 91.1 MB (90.38%) - LeetHub

--- 0716-max-stack/0716-max-stack.java
+++ 0716-max-stack/0716-max-stack.java
@@ -1,3 +1,4 @@
+/*
 class MaxStack {
     TreeSet<int[]> st;
     TreeSet<int[]> value;
@@ -34,6 +35,58 @@ class MaxStack {
         int[] p = value.pollLast();
         st.remove(new int[]{p[1], p[0]});
         return p[0];
+    }
+}
+*/
+class MaxStack {
+    private Stack<int[]> stack;
+    private Queue<int[]> heap;
+    private Set<Integer> removed;
+    private int cnt;
+
+    public MaxStack() {
+        stack = new Stack<>();
+        heap = new PriorityQueue<>((a, b) -> b[0] - a[0] == 0 ? b[1] - a[1] : b[0] - a[0]);
+        removed = new HashSet<>();
+    }
+
+    public void push(int x) {
+        stack.add(new int[] { x, cnt });
+        heap.add(new int[] { x, cnt });
+        cnt++;
+    }
+
+    public int pop() {
+        while (removed.contains(stack.peek()[1])) {
+            stack.pop();
+        }
+        int[] top = stack.pop();
+        removed.add(top[1]);
+        return top[0];
+    }
+
+    public int top() {
+        while (removed.contains(stack.peek()[1])) {
+            stack.pop();
+        }
+        return stack.peek()[0];
+    }
+
+    public int peekMax() {
+        while (removed.contains(heap.peek()[1])) {
+            heap.poll();
+        }
+        return heap.peek()[0];
+
+    }
+
+    public int popMax() {
+        while (removed.contains(heap.peek()[1])) {
+            heap.poll();
+        }
+        int[] top = heap.poll();
+        removed.add(top[1]);
+        return top[0];
     }
 }
 
