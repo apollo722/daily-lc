cd05f4435b4672eff739eb28b9039ace65687cec
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-12 16:43:03 -0700

    Time: 94 ms (28.99%), Space: 96.4 MB (33.43%) - LeetHub

---
+++ 2357-count-integers-in-intervals/2357-count-integers-in-intervals.java
@@ -0,0 +1,18 @@
+class CountIntervals {
+    TreeMap<Integer, Integer> m = new TreeMap<>();
+    int cnt = 0;
+    public void add(int left, int right) {
+        var it = m.floorEntry(left);
+        if (it == null || it.getValue() < left)
+            it = m.higherEntry(left);
+        for (; it != null && it.getKey() <= right; it = m.higherEntry(left)) {
+            left = Math.min(left, it.getKey());
+            right = Math.max(right, it.getValue());
+            cnt -= it.getValue() - it.getKey() + 1;
+            m.remove(it.getKey());
+        }
+        m.put(left, right);
+        cnt += right - left + 1;        
+    }
+    public int count() { return cnt; }
+}
