a4572bff1cdc6159dfec6ca481495236914fbe4b
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-12 19:35:54 -0700

    Time: 29 ms (88.91%), Space: 46.8 MB (28.64%) - LeetHub

--- 1905-design-authentication-manager/1905-design-authentication-manager.java
+++ 1905-design-authentication-manager/1905-design-authentication-manager.java
@@ -1,24 +1,60 @@
 class AuthenticationManager {
 
-    HashMap<String, Integer> map;
-    int time;
+    private static int ttl;
+    HashMap<String, Integer> tokenTTLMap;
+    Queue<String> queue;
+
     public AuthenticationManager(int timeToLive) {
-        map = new HashMap<>();
-        time = timeToLive;
+        this.tokenTTLMap = new HashMap<>();
+        this.queue = new ArrayDeque<>();
+        this.ttl = timeToLive;
+    }
+    
+    private void removeExpired(int currentTime) {
+
+        while(!queue.isEmpty()) {
+            String token = queue.peek();
+            
+            if(tokenTTLMap.containsKey(token) && tokenTTLMap.get(token) > currentTime) {
+                break;
+            } 
+
+            queue.poll();
+            tokenTTLMap.remove(token);
+        }
+
     }
 
     public void generate(String tokenId, int currentTime) {
-        map.put(tokenId, currentTime + time);
-    }
 
+        if(!this.tokenTTLMap.containsKey(tokenId)) {
+            this.queue.add(tokenId);
+        }
+
+        this.tokenTTLMap.put(tokenId, currentTime + this.ttl);
+        this.removeExpired(currentTime);
+    }
+    
     public void renew(String tokenId, int currentTime) {
-        int expires = map.getOrDefault(tokenId, 0);
-        if (expires > currentTime) map.put(tokenId, currentTime + time);
+        if(this.tokenTTLMap.containsKey(tokenId) && this.tokenTTLMap.get(tokenId) > currentTime) {
+            this.tokenTTLMap.put(tokenId, currentTime + this.ttl);
+            this.queue.remove(tokenId);
+            this.queue.add(tokenId);
+        }   
+        this.removeExpired(currentTime);
     }
-
+    
     public int countUnexpiredTokens(int currentTime) {
-        int count = 0;
-        for (String key : map.keySet()) if (map.get(key) > currentTime) count++;
-        return count;
+        this.removeExpired(currentTime);
+        
+        return queue.size();
     }
 }
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager obj = new AuthenticationManager(timeToLive);
+ * obj.generate(tokenId,currentTime);
+ * obj.renew(tokenId,currentTime);
+ * int param_3 = obj.countUnexpiredTokens(currentTime);
+ */
