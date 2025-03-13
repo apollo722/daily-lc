6ec5bd1725a0a21c5f8451c8907407b39f4ce869
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-12 17:00:18 -0700

    Time: 20 ms (28.61%), Space: 45.3 MB (53.86%) - LeetHub

---
+++ 1186-building-h2o/1186-building-h2o.java
@@ -0,0 +1,26 @@
+class H2O {
+    
+    private final Semaphore hydrogenSemaphore;
+    private final Semaphore oxygenSemaphore;
+
+    public H2O() {
+        hydrogenSemaphore = new Semaphore(2);
+        oxygenSemaphore = new Semaphore(0); 
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        hydrogenSemaphore.acquire();
+
+        releaseHydrogen.run();
+
+        oxygenSemaphore.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        oxygenSemaphore.acquire(2);
+
+        releaseOxygen.run();
+
+        hydrogenSemaphore.release(2);
+    }
+}
