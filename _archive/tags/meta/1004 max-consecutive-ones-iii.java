31ded2bfc688370adaefcdb14ae7b7c8def289db
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-23 14:46:27 -0700

    Update 1004 max-consecutive-ones-iii.java

--- _archive/tags/meta/1004 max-consecutive-ones-iii.java
+++ _archive/tags/meta/1004 max-consecutive-ones-iii.java
@@ -24,3 +24,54 @@ class Solution {
         return r - l;
     }
 }
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int[] extendedNums = new int[2 * n];
+        System.arraycopy(nums, 0, extendedNums, 0, n);
+        System.arraycopy(nums, 0, extendedNums, n, n);
+
+        int l = 0, r = 0;
+        int maxLen = 0;
+
+        while (r < 2 * n) {
+            if (extendedNums[r] == 0) k--;
+            if (k < 0) {
+                if (extendedNums[l] == 0) k++;
+                l++;
+            }
+            maxLen = Math.max(maxLen, r - l + 1);
+            r++;
+        }
+
+        return Math.min(maxLen, n); // 最大长度不会超过原数组长度
+    }
+}
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int l = 0, r = 0;
+        int maxLen = 0;
+
+        while (r < n) {
+            if (nums[r] == -1) {
+                l = r + 1; // 遇到-1，移动左指针到r+1
+                k = k; // 重置k，因为-1不能被翻转
+            } else if (nums[r] == 0) {
+                k--;
+            }
+
+            if (k < 0) {
+                if (nums[l] == 0) k++;
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r - l + 1);
+            r++;
+        }
+
+        return maxLen;
+    }
+}
