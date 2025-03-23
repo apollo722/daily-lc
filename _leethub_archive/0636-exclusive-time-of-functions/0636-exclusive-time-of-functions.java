58adcae55180bb2ea77e1c8b5e825599b6e0342f
apollo722 <135302413+apollo722@users.noreply.github.com>
2025-03-23 14:49:34 -0700

    Update 0636-exclusive-time-of-functions.java

--- _leethub_archive/0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
+++ _leethub_archive/0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -1,20 +1,22 @@
 class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
-        int[] res = new int[n];
-        Deque<int[]> st = new ArrayDeque<>();
-        for (String s : logs) {
-            String[] arr = s.split(":");
-            int id = Integer.valueOf(arr[0]), time = Integer.valueOf(arr[2]);
-            boolean isStart = arr[1].equals("start");
+        int[] result = new int[n];
+        Deque<int[]> startTimes = new ArrayDeque<>();
+        for (String input : logs) {
+            String[] inputArr = input.split(":");
+            int funcNum = Integer.valueOf(inputArr[0]), curTime = Integer.valueOf(inputArr[2]);
+            boolean isStart = inputArr[1].equals("start");
             if (isStart) {
-                st.addLast(new int[]{id, time});
+                startTimes.addLast(new int[]{funcNum, curTime});
             } else {
-                int[] prev = st.pollLast();
-                int addTime = time - prev[1] + 1;
-                if (!st.isEmpty()) res[st.peekLast()[0]] -= addTime;
-                res[prev[0]] += addTime;
+                int[] prevAction = startTimes.pollLast();
+                int execTime = curTime - prevAction[1] + 1;
+                if (!startTimes.isEmpty()) {
+                    result[startTimes.peekLast()[0]] -= execTime;
+                }
+                result[funcNum] += execTime;
             }
         }
-        return res;
+        return result;
     }
 }
